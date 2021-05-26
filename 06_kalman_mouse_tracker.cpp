542adeee6b03f93b4d056ee5189873c972f19c23
Milind Deore <tomdeore@gmail.com>
2021-05-27 02:32:52 +0530

    Update 06_kalman_mouse_tracker.cpp
    Formating and indentations.

--- 06_kalman_mouse_tracker.cpp
+++ 06_kalman_mouse_tracker.cpp
@@ -37,69 +37,68 @@ cv::Mat new_image()
 
 class EKF
 {
-	private:
-		const float pval = 0.1;
-	    const float qval = 0.0001;
-	    const float rval = 0.1;
+    private:
+	const float pval = 0.1;
+	const float qval = 0.0001;
+	const float rval = 0.1;
         uint32_t height;
         uint32_t width;
 
-		// No previous prediction noise covariance
-		cv::Mat P_pre;
-		cv::Mat x;
-		cv::Mat P_post;
-		cv::Mat Q;
-		cv::Mat R;
-		cv::Mat I;
+	// No previous prediction noise covariance
+	cv::Mat P_pre;
+	cv::Mat x;
+	cv::Mat P_post;
+	cv::Mat Q;
+	cv::Mat R;
+	cv::Mat I;
 
-		cv::Mat F;
-		cv::Mat h;
-		cv::Mat H;
-		cv::Mat G;
+	cv::Mat F;
+	cv::Mat h;
+	cv::Mat H;
+	cv::Mat G;
 
-	public:
+    public:
 
-		//
-		// Constructor 
-		//
-		EKF(uint32_t n, uint32_t m)
-		{
+	//
+	// Constructor 
+	//
+	EKF(uint32_t n, uint32_t m)
+	{
             width = n;
             height = m;
+	    x = cv::Mat::zeros(1, n, CV_32FC1);
+	    P_post = cv::Mat::eye(n, n, CV_32FC1) * pval;
 
-			x = cv::Mat::zeros(1, n, CV_32FC1);
-			P_post = cv::Mat::eye(n, n, CV_32FC1) * pval;
+	    Q = cv::Mat::eye(n, n, CV_32FC1) * qval;
+	    R = cv::Mat::eye(m, m, CV_32FC1) * rval;
 
-			Q = cv::Mat::eye(n, n, CV_32FC1) * qval;
-			R = cv::Mat::eye(m, m, CV_32FC1) * rval;
+	    I = cv::Mat::eye(n, n, CV_32FC1);
+	}
 
-			I = cv::Mat::eye(n, n, CV_32FC1);
-		}
+	// 
+	// Step 
+	//
 
-		// 
-		// Step 
-		//
-
-		cv::Mat step(float xx, float yy)
-		{
+	cv::Mat step(float xx, float yy)
+	{
             float xxyy[] = {xx, yy};
             cv::Mat z(1, 2, CV_32FC1, xxyy);
 
-			// Predict
-			F = cv::Mat::eye(2, 2, CV_32FC1);
-			P_pre = F * P_post * F.t() + Q;
+	    // Predict
+	    F = cv::Mat::eye(2, 2, CV_32FC1);
+	    P_pre = F * P_post * F.t() + Q;
 
-			// Update
-			x.copyTo(h);
-			H = cv::Mat::eye(2, 2, CV_32FC1);	
+	    // Update
+	    x.copyTo(h);
+	    H = cv::Mat::eye(2, 2, CV_32FC1);	
 
-			cv::Mat H_P_pre_R =  H * P_pre * H.t() + R; 
-			G =  P_pre * H.t() * H_P_pre_R.inv(); 
-			x += (z - h) * G;
-			P_post = (I - (G * H)) * P_pre;
+	    cv::Mat H_P_pre_R =  H * P_pre * H.t() + R; 
+	    G =  P_pre * H.t() * H_P_pre_R.inv(); 
+	    x += (z - h) * G;
+	    P_post = (I - (G * H)) * P_pre;
 
-			return x;
-		}
+	    return x;
+	}
 };
 
 
